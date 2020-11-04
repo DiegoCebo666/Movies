1c25888d830d77f656c089fce5c11851eb525f78
Diego <diegocebo7@gmail.com>
2020-11-04 10:52:52 +0100

    Ejercicio 1

---
+++ movies/src/test/java/com/lab1diegocebo/movies/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab1diegocebo.movies.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
