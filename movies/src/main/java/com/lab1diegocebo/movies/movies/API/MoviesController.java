e630fbc7f676c172ca8c85cf95f23a2d1371c32c
Diego <diegocebo7@gmail.com>
2020-11-07 19:03:27 +0100

    POST, PUT, and DELETE methods added

--- movies/src/main/java/com/lab1diegocebo/movies/movies/API/MoviesController.java
+++ movies/src/main/java/com/lab1diegocebo/movies/movies/API/MoviesController.java
@@ -6,18 +6,20 @@ import java.util.List;
 
 import com.lab1diegocebo.movies.movies.Model.Movie;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MoviesController {
 
-    private static List<Movie> movies = Arrays.asList(new Movie(1, "Pipo", 1970,
-            "https://m.media-amazon.com/images/M/MV5BNzhiMzNlOTctZDFjOS00MDlhLTk1ODAtNjM3ZmJjMTgwNmFhXkEyXkFqcGdeQXVyMDQ2MzA4Mw@@._V1_UY268_CR80,0,182,268_AL_.jpg"),
-            new Movie(2, "404: Error Not Found", 2011,
-            "https://m.media-amazon.com/images/M/MV5BMjc0YWM3MjQtNzNhYy00ZWM5LTk2ODMtNTE0ZmVmYzgzMWMwXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY268_CR3,0,182,268_AL_.jpg"));
-
+    private static ArrayList<Movie> movies = new ArrayList<>();
+    
     @GetMapping("/movies")
     public List<Movie> getMovies(@RequestParam(name="title", required=false, defaultValue="") String title,
                         @RequestParam(name="year", required = false, defaultValue = "0") int year) {
@@ -54,5 +56,41 @@ public class MoviesController {
             }
         }
         return moviesByYear;
+    }
+
+    @PostMapping("movies")
+    public Movie Add(@RequestBody Movie newMovie) {
+        movies.add(newMovie);
+        return newMovie;
+    }
+
+    @PutMapping("movies/{id}")
+    public Movie Update(@RequestBody Movie updateMovie, @PathVariable("id") int id) {
+        for (int i = 0; i < movies.size(); i++) {
+            if (movies.get(i).getId() == id) {
+                if(!updateMovie.getTitle().equals("")){
+                    movies.get(i).setTitle(updateMovie.getTitle());
+                }
+                if(updateMovie.getYear() != 0){
+                    movies.get(i).setYear(updateMovie.getYear());
+                }
+                if(!updateMovie.getImg().equals("")){
+                    movies.get(i).setImg(updateMovie.getImg());
+                }
+                return updateMovie;
+            }
+        }
+        return updateMovie;
+    }
+
+    @DeleteMapping("movies/{id}")
+    public int Delete(@PathVariable("id") int id){
+        for (int i = 0; i < movies.size(); i++) {
+            if(movies.get(i).getId() == id){
+                movies.remove(i);
+                return 1;
+            }
+        }
+        return 0;
     }
 }
